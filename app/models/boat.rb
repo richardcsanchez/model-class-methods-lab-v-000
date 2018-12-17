cedd1c1b6210d7b51b18217005f50e1142511e51
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-17 17:45:20 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -26,4 +26,9 @@ class Boat < ActiveRecord::Base
   def self.sailboats
     includes('classifications').where(classifications: {name: 'Sailboat'})
   end
+
+  def self.with_three_classifications
+    includes('classifications').where(classifications.length = 3)
+  end
+
 end
