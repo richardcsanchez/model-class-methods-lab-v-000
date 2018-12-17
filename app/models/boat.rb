868141853539700febac1d2aea1c6fbec23e80c1
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-17 17:43:00 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,6 +24,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.sailboats
-    where(classification: "Sailboats")
+    includes('classifications').where(classifications: {name: 'Sailboat'})
   end
 end
