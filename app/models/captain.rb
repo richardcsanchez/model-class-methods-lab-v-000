770f5c620a632e151f042e684b0b2e2f3d1115b5
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-17 17:56:04 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,8 +1,8 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+  has_many :classifications, through: :boats
 
   def self.catamaran_operators
-    includes('boats').where(classifications: {name: 'Catamaran'})
-
+    includes(boats: :classifications).where(classifications: {name: 'Catamaran'})
   end
 end
